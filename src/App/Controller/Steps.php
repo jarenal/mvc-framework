38a3964bff2878fc06d4888bca5cbc406b6e8a91
Jose Antonio <jguidos@hotmail.com>
2019-09-17 10:37:49 +0100

    Rename templates folder from 'home' to 'steps'

--- src/App/Controller/Steps.php
+++ src/App/Controller/Steps.php
@@ -2,21 +2,29 @@
 
 namespace Jarenal\App\Controller;
 
+use Jarenal\App\Model\ProductQueries;
 use Jarenal\App\Model\Quote;
 use Jarenal\App\Model\User;
+use Jarenal\Core\Config;
 use Jarenal\Core\ControllerAbstract;
+use Jarenal\Core\Database;
+use mysqli;
 
 class Steps extends ControllerAbstract
 {
     public function step1()
     {
-        return $this->view->render("home/step1.tpl", ["title" => "1- Enter your personal data"]);
+        return $this->view->render("steps/step1.tpl", ["title" => "1- Enter your personal data"]);
     }
 
     public function step2()
     {
         $this->session->set("user", $_POST["user"]);
-        return $this->view->render("home/step2.tpl", ["title" => "2- Choose your products"]);
+        $config = new Config(PROJECT_ROOT_DIR."/config/config.yaml");
+        $database = new Database(new mysqli(), $config);
+        $productQueries = new ProductQueries($database);
+        $products = $productQueries->findAll();
+        return $this->view->render("steps/step2.tpl", ["title" => "2- Choose your products", "products" => $products]);
     }
 
     public function step3()
@@ -37,6 +45,6 @@ class Steps extends ControllerAbstract
             ->setTotal(123.45)
             ->save();
 
-        return $this->view->render("home/step3.tpl", ["title" => "3- Thank you!"]);
+        return $this->view->render("steps/step3.tpl", ["title" => "3- Thank you!"]);
     }
 }
